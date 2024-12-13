23d296f7a9fc958adffbdfb09436f9b0239d9c37
Edward Vidal Jr <develone@sbcglobal.net>
2024-12-13 14:44:44 -0700

    here  ok. . 7574  ok. lo-pass-fwd-1 . 78  ok. lo-pass-fwd-2 lo-pass-fwd-1  ok. sp_a offset @ CELLS + @ . 77  ok.

--- common-forth/test.fs
+++ common-forth/test.fs
@@ -1,10 +1,24 @@
 : test 200 0 do i depth . loop  ; 
-variable lp 
-variable rp 
-variable sp 
-155 sp ! 
-156 rp ! 
-157 lp !
-: lo-pass-fwd-1 rp @ 2/  ;
-: lo-pass-fwd-2 sp @ lp @ 2/  - sp ! ; 
-lo-pass-fwd-2 lo-pass-fwd-1 
+variable lp_a 3 CELLS ALLOT
+variable rp_a 3 CELLS ALLOT
+variable sp_a 3 CELLS ALLOT
+variable offset 
+0 offset ! 
+: lo-pass-fwd-1 rp_a offset @ CELLS + @ 2/  ;
+: lo-pass-fwd-2 sp_a offset @ CEllS + @ lp_a offset @ CELLS + @ 2/  - sp_a offset @ CELLS + ! ;
+
+157 lp_a 0 CELLS + ! 
+156 rp_a 0 CELLS + !
+155 sp_a 0 CELLS + !
+
+157 lp_a 1 CELLS + ! 
+155 rp_a 1 CELLS + !
+154 sp_a 1 CELLS + !
+
+156 lp_a 2 CELLS + ! 
+156 rp_a 2 CELLS + !
+155 sp_a 2 CELLS + !
+
+154 lp_a 3 CELLS + ! 
+157 rp_a 3 CELLS + !
+155 sp_a 3 CELLS + !
