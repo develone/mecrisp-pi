a0ebf0cdcbe293db25524ad6642d3d00606821f2
Edward Vidal Jr <develone@sbcglobal.net>
2024-12-14 12:00:43 -0700

    cat ../common-forth/coredefinitions.fs basisdefinitions.fs ../common/double.fs ../common/fixpoint.fs ../common-forth/insight-16kb.fs ../common-forth/lo-pass.fs > included.fs
    ../common-crosscompiler/icecreammachine-16kb build/nucleus.hex build/iceimage.hex included.fs > build/log.txt
    
    ./compile
    
    bin2uf2 -o gateware.uf2 j1a.bin
    
    scp gateware.uf2 pi5-80:/media/devel/pico-ice/
    
    double click Desktop/picocom.sh

--- common-forth/lo-pass.fs
+++ common-forth/lo-pass.fs
@@ -1,9 +1,30 @@
-variable lp 
-variable rp 
-variable sp 
-155 sp ! 
-156 rp ! 
-157 lp !
-: lo-pass-fwd-1 rp @ 2/  ;
-: lo-pass-fwd-2 sp @ lp @ 2/  - sp ! ; 
-lo-pass-fwd-2 lo-pass-fwd-1 
+: test 200 0 do i depth . loop  ; 
+variable lp_a 3 CELLS ALLOT
+variable rp_a 3 CELLS ALLOT
+variable sp_a 3 CELLS ALLOT
+variable offset 
+0 offset ! 
+: lo-pass-fwd-1 lp_a offset @ CELLS + @ 2/ rp_a offset @ CELLS + @ 2/ + ;
+: lo-pass-fwd-2 sp_a offset @ CEllS + @ - sp_a offset @ CELLS + ! ;
+: lo-pass-inv-1 lp_a offset @ CELLS + @ 2/ rp_a offset @ CELLS + @ 2/ + ;
+: lo-pass-inv-2 lp_a offset @ CELLS + @ 2/ rp_a offset @ CELLS + @ 2/ + sp_a offset @ CEllS + @ swap + ;
+: lo-fwd lo-pass-fwd-2 lo-pass-fwd-1 ;
+: lo-inv lo-pass-inv-2 lo-pass-inv-1 sp_a offset @ CELLS + ! ;
+
+157 lp_a 0 CELLS + ! 
+156 rp_a 0 CELLS + !
+155 sp_a 0 CELLS + !
+
+157 lp_a 1 CELLS + ! 
+155 rp_a 1 CELLS + !
+154 sp_a 1 CELLS + !
+
+156 lp_a 2 CELLS + ! 
+156 rp_a 2 CELLS + !
+155 sp_a 2 CELLS + !
+
+154 lp_a 3 CELLS + ! 
+157 rp_a 3 CELLS + !
+155 sp_a 3 CELLS + !
+
+ 
